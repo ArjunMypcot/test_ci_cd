b7f88cc7eac844d46abc4809a3e76e587839db6e
Arjun singh <chhotelal.s@mypcot.com>
2024-05-08 15:41:21 +0530

    Modified the text file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,14 +5,14 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class Example extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function example()
     {
         $response = $this->get('/');
 
